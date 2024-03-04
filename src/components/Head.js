4867e9b0d06f27187925ba8a708b15ecb09470fb
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-05 02:46:28 +0530

    config done tailwindcss

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Head = () => {
   return (
-    <div>Head</div>
-  )
-}
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg ">
+      <div className="flex col-span-1 justify-between">
+        <img
+          className="h-8"
+          src="https://icon-library.com/images/hamburger-menu-icon/hamburger-menu-icon-16.jpg"
+          alt="menu"
+        />
+        <img
+          className="h-8"
+          src="https://vectorseek.com/wp-content/uploads/2021/01/YouTube-Logo-Vector.png"
+          alt="youtube-logo"
+        />
+      </div>
+      <div className="col-span-10">
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://static.vecteezy.com/system/resources/previews/019/879/198/non_2x/user-icon-on-transparent-background-free-png.png"
+          alt="user-icon"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Head
+export default Head;
