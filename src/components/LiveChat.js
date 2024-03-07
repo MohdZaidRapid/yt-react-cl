fce152485c038013da063c165ddaef070fc602ee
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-07 15:41:58 +0530

    added

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chat.messages);
 
@@ -23,12 +24,37 @@ const LiveChat = () => {
     return () => clearInterval(clearInteval);
   }, []);
   return (
-    <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg  overflow-y-scroll">
-      {/* don't use indexes as keys */}
-      {chatMessages?.map((c, index) => (
-        <ChatMessage key={index} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg  overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {/* don't use indexes as keys */}
+          {chatMessages?.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="w-full p-2 ml-2 border border-black rounded-lg"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Hello word");
+          dispatch(
+            addMessage({
+              name: "Mohd Zaid",
+              message: liveMessage,
+            })
+          );
+        }}
+      >
+        <input
+          className="px-2 w-96"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button className="px-2 mx-2 bg-green-100 rounded-lg">Send</button>
+      </form>
+    </>
   );
 };
 
