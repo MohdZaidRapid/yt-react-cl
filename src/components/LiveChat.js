33e8603a69d87f150bb017f13995b2cba3062c4d
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-07 14:51:52 +0530

    added

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+    
+  useEffect(() => {
+    const clearInteval = setInterval(() => {
+      // API Polling
+      console.log("API Polling");
+    }, 2000);
+
+    return () => clearInterval(clearInteval);
+  }, []);
   return (
     <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage name="zaid" message="This is zaid live youtube stream 🚀" />
-      <ChatMessage name="zaid" message="This is zaid live youtube stream 🚀" />
-      <ChatMessage name="zaid" message="This is zaid live youtube stream 🚀" />
-      <ChatMessage name="zaid" message="This is zaid live youtube stream 🚀" />
       <ChatMessage name="zaid" message="This is zaid live youtube stream 🚀" />
     </div>
   );
